43400d3aa92f7db83a507c4f8e8c02e026c9f156
Woodiik <vovikvova04@gmail.com>
2023-01-15 13:16:39 +0200

    updated

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,19 +1,15 @@
 const itemsEl = document.querySelector('#categories').children;
 console.log("Number of categories:", itemsEl.length);
 
-const categoryNameEl = document.querySelectorAll('.category-title');
+[...itemsEl].map(item => {
+   console.log(`Category: ${item.firstElementChild.textContent}`)
+   console.log(`Elements: ${item.lastElementChild.children.length}`)
+})
 
-const categoryElements = document.querySelectorAll('.item-list');
 
 
-console.log('Category:', categoryNameEl[0].textContent);
-console.log('Elements:', categoryElements[0].children.length);
 
-console.log('Category:', categoryNameEl[1].textContent);
-console.log('Elements:', categoryElements[1].children.length);
 
-console.log('Category:', categoryNameEl[2].textContent);
-console.log('Elements:', categoryElements[2].children.length);
 
 
 
